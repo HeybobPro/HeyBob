6d01ceeb5bd9248a0047a23845d5270ff42e06b1
Heybob <104833565+HeybobPro@users.noreply.github.com>
2022-05-11 19:38:20 +0200

    Add files via upload

---
+++ js/JavaScript.js
@@ -0,0 +1,34 @@
+
+  
+      function revealHR() {
+        var reveals = document.querySelectorAll(".revealHR");
+      
+        for (var i = 0; i < reveals.length; i++) {
+          var windowHeight = window.innerHeight;
+          var elementTop = reveals[i].getBoundingClientRect().top;
+          var elementVisible = 170;
+      
+          if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+          } else {
+            reveals[i].classList.remove("active");
+          }
+        }
+      }
+      function revealHL() {
+      var reveals = document.querySelectorAll(".revealHL");
+      
+      for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 170;
+    
+        if (elementTop < windowHeight - elementVisible) {
+          reveals[i].classList.add("active");
+        } else {
+          reveals[i].classList.remove("active");
+        }
+      }
+    }
+      window.addEventListener("scroll", revealHR);
+      window.addEventListener("scroll", revealHL);
